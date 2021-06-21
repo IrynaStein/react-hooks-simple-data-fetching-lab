4acc234d97bb0c74de0e68c6662eff2555531b3c
Iryna Stein <isophiestein@gmail.com>
2021-06-21 12:05:14 -0600

    commit tree 203fc45bbb59e9ca0edc7202cdb0e22707b881ce

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import { useEffect, useState } from "react"
+function App() {
+    const [dogs, setDogs] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const URL = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(() => {
+        fetch(URL)
+            .then(resp => resp.json())
+            .then(data => {
+                setDogs(data.message)
+                setIsLoaded(true)
+            })
+
+    }, [])
+
+if(!isLoaded) return <p>Loading...</p>
+
+
+    return (
+        <div>
+            <img alt="A Random Dog" src={dogs} />
+        </div>
+    )
+}
+export default App;
